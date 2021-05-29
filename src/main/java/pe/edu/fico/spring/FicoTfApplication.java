60543385f4498e745752539881a16274c8fa9309
joanmach <dominio46@hotmail.com>
2021-05-29 16:17:42 -0500

    probando

---
+++ src/main/java/pe/edu/fico/spring/FicoTfApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.fico.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FicoTfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FicoTfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/fico/spring/FicoTfApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.fico.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FicoTfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
