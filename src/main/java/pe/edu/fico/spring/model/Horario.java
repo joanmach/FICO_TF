192a276667e27029ec26a3a4bc0ce622c227b0c8
joanmach <dominio46@hotmail.com>
2021-06-15 21:09:38 -0500

    actualizacion

--- src/main/java/pe/edu/fico/spring/model/Horario.java
+++ src/main/java/pe/edu/fico/spring/model/Horario.java
@@ -25,37 +25,37 @@ public class Horario {
 	private int CHorario;
 	
 	@Temporal(TemporalType.DATE)
-	@Column(name="fechaInicio",nullable=false, length = 50)
+	@Column(name="FechaInicio",nullable=false, length = 50)
 	@DateTimeFormat(pattern= "yyyy-MM-dd")
 	private Date FechaInicio;
 	
 	@Temporal(TemporalType.DATE)
-	@Column(name="fechaFin",nullable=false, length = 50)
+	@Column(name="FechaFin",nullable=false, length = 50)
 	@DateTimeFormat(pattern= "yyyy-MM-dd")
 	private Date FechaFin;
 	
-//	@Temporal(TemporalType.TIME)
-//	@Column(name="horaInicio",nullable=false, length = 50)
-//	@DateTimeFormat(pattern= "HH:mm")
-//	private Date HoraInicio;
-//	
-//	@Temporal(TemporalType.TIME)
-//	@Column(name="horaFin",nullable=false, length = 50)
-//	@DateTimeFormat(pattern= "HH:mm")
-//	private Date HoraFin;
+	@Column(name="HoraInicio",nullable=false, length = 50)
+	@Temporal(TemporalType.TIME)
+    @DateTimeFormat(pattern= "HH:mm")
+	private Date HoraInicio;
+		
+	@Column(name="HoraFin",nullable=false, length = 50)
+	@Temporal(TemporalType.TIME)
+	@DateTimeFormat(pattern= "HH:mm")
+	private Date HoraFin;
 
 	public Horario() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public Horario(int cHorario, Date fechaInicio, Date fechaFin /*, Date horaInicio, Date horaFin*/) {
+	public Horario(int cHorario, Date fechaInicio, Date fechaFin , Date horaInicio, Date horaFin) {
 		super();
 		CHorario = cHorario;
 		FechaInicio = fechaInicio;
 		FechaFin = fechaFin;
-//		HoraInicio = horaInicio;
-//		HoraFin = horaFin;
+		HoraInicio = horaInicio;
+		HoraFin = horaFin;
 	}
 	public int getCHorario() {
 		return CHorario;
@@ -81,21 +81,21 @@ public class Horario {
 		FechaFin = fechaFin;
 	}
 
-//	public Date getHoraInicio() {
-//		return HoraInicio;
-//	}
-//
-//	public void setHoraInicio(Date horaInicio) {
-//		HoraInicio = horaInicio;
-//	}
-//
-//	public Date getHoraFin() {
-//		return HoraFin;
-//	}
-//
-//	public void setHoraFin(Date horaFin) {
-//		HoraFin = horaFin;
-//	}
+	public Date getHoraInicio() {
+		return HoraInicio;
+	}
+
+	public void setHoraInicio(Date horaInicio) {
+		HoraInicio = horaInicio;
+	}
+
+	public Date getHoraFin() {
+		return HoraFin;
+	}
+
+	public void setHoraFin(Date horaFin) {
+		HoraFin = horaFin;
+	}
 	
 	
 	
